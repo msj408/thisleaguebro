a6c5f7a10345bc12d59bfbc41356149c8a7eb689
msj408 <manbir410@gmail.com>
2024-12-19 16:55:08 -0800

    update tweet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="container">
-      <Navbar />
-    </div>
-  );
+  return function App() {
+    return (
+      <>
+        <div>
+          <h1>Dummy Tweet</h1>
+          <p>This is a test tweet for generating previews.</p>
+        </div>
+      </>
+    );
+  };
 }
 
 export default App;
